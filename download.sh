ade51f32abe67141c6d6d6abb8a4fb0537301afc
Tom Le <thongld2012@gmail.com>
2019-04-21 03:19:06 -0700

    Fix download

--- download.sh
+++ download.sh
@@ -14,7 +14,7 @@ fshare_download() {
     -H 'Accept-Encoding: gzip, deflate, br' \
     -H 'Content-Type: application/json' \
     -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36' \
-    -d $fshare_download_data --compressed | gawk 'match($0, /"location"\:"(.+?)"/, group) {print group[1]}')
+    -d $fshare_download_data --compressed | gawk 'match($0, /"location"\:"(.+?)"/, group) {print group[1]}' | sed -e 's/\\//g')
   local red="\033[0;31m"
   local green="\033[0;32m"
   local nc="\e[0m"
