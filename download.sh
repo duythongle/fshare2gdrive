0b1c8fcfdfeddeeffa57aee78b3a5bd6f5ff3727
Tom Le <thongld2012@gmail.com>
2019-04-21 01:26:32 -0700

    try 19

--- download.sh
+++ download.sh
@@ -9,15 +9,15 @@ fshare_download() {
   local fshare_session_id=$(echo $fshare_login_result | gawk 'match($0, /"session_id"\:"(\w+)"/, group) {print group[1]}')
   local fshare_download_template='{"url":"%s","token":"%s","password":""}'
   local fshare_download_data=$(printf "$fshare_download_template" "$fshare_file_url" "$fshare_token")
-  echo $fshare_download_data
-  echo $fshare_session_id
   local extracted_download_url=$(curl -s -4 $fshare_download_api \
     -H 'Cookie: session_id='$fshare_session_id \
     -H 'Accept-Encoding: gzip, deflate, br' \
     -H 'Content-Type: application/json' \
     -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36' \
     -d $fshare_download_data --compressed | gawk 'match($0, /"location"\:"(.+?)"/, group) {print group[1]}')
+  echo 'Extracted download url: '$extracted_download_url
   local download_file_name=$(echo $extracted_download_url | gawk 'match($0, /.+\/(.+?)$/, group) {print group[1]}')
+  echo 'Uploading to: '$rclone_remote_name':'$remote_folder_path$download_file_name
   curl -s $extracted_download_url | \
     rclone rcat $rclone_remote_name:$remote_folder_path$download_file_name
 }
