3210fb5f98c0edbadc2cb2033cc1d4f778fc579a
Tom Le <thongld2012@gmail.com>
2019-05-09 02:42:21 -0700

    fix url decode

--- download.sh
+++ download.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+urlencode() {
+    # urlencode <string>
+    local length="${#1}"
+    for (( i = 0; i < length; i++ )); do
+        local c="${1:i:1}"
+        case $c in
+            [a-zA-Z0-9.~_-]) printf "$c" ;;
+            *) printf '%%%02X' "'$c" ;;
+        esac
+    done
+}
+
 urldecode() {
     # urldecode <string>
 
