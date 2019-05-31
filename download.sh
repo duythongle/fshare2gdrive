d455a7b06222c42c55906d6dc919be46ba13a9a9
Tom Le <thongld2012@gmail.com>
2019-05-31 13:00:28 -0700

    Fix #3

--- download.sh
+++ download.sh
@@ -17,7 +17,7 @@ fshare_download() {
     -H 'Content-Type: application/json' \
     -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36' \
     -d $fshare_download_data --compressed | gawk 'match($0, /"location"\:"(.+?)"/, group) {print group[1]}' | sed -e 's/\\//g')
-  extracted_download_url=$(python -c "import sys, urllib as ul; print ul.unquote_plus(\""$extracted_download_url\"")")
+  extracted_download_url=$(python -c "import sys, urllib as ul; print ul.unquote(\""$extracted_download_url\"")")
   local red="\033[0;31m"
   local green="\033[0;32m"
   local nc="\e[0m"
